5844da523470c5d8f23fa9654c24e5e55022a32a
Nico P. <143935315+nicolux2006@users.noreply.github.com>
2026-08-31 13:59:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+try
+{
+    if (args.Length == 0)
+    {
+        Environment.ExitCode = 1;
+        return;
+    }
+
+    string scriptPath = args[0];
+
+    if (!File.Exists(scriptPath))
+    {
+        Environment.ExitCode = 1;
+        return;
+    }
+
+    StringBuilder psArgs = new();
+    psArgs.Append("-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden ");
+    psArgs.Append($"-File \"{scriptPath}\"");
+
+    for (int i = 1; i < args.Length; i++)
+    {
+        psArgs.Append($" \"{args[i]}\"");
+    }
+
+    ProcessStartInfo psi = new()
+    {
+        FileName = "powershell.exe",
+        Arguments = psArgs.ToString(),
+        UseShellExecute = false,
+        CreateNoWindow = true,
+        WindowStyle = ProcessWindowStyle.Hidden
+    };
+
+    using Process? process = Process.Start(psi);
+
+    if (process is null)
+    {
+        Environment.ExitCode = 1;
+        return;
+    }
+
+    process.WaitForExit();
+
+    Environment.ExitCode = process.ExitCode;
+}
+catch
+{
+    Environment.ExitCode = 1;
+}
